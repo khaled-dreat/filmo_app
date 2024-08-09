3813baf26ffd8e6946d7e6801c15aaa155366ee6
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-09 18:04:16 +0300

    Add app icons constants for mail, password, and visibility toggles
    This commit introduces a new file, `app_icons.dart`, within the `lib/utils/constant` directory. The file defines a class `AppIcons` that encapsulates static constants for commonly used icons in the application. Specifically, it includes icons for mail, password, and toggles for showing and hiding passwords. These icons are sourced from the `CupertinoIcons` package, providing a consistent and easily accessible way to reference these icons throughout the application. This addition aims to streamline the process of icon usage and enhance code readability by centralizing icon definitions.

---
+++ lib/utils/constant/app_icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+class AppIcons {
+  static IconData mail = CupertinoIcons.mail_solid;
+  static IconData pass = CupertinoIcons.lock_fill;
+  static IconData showPass = CupertinoIcons.eye_fill;
+  static IconData noShowPass = CupertinoIcons.eye_slash_fill;
+}
