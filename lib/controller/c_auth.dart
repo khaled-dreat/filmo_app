d8b732e110ebda4b284fb42755897149d599342d
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-10 16:51:13 +0300

    Refactor authentication controller to integrate The Movie Database API
    - Added methods to create a request token and session for authentication using The Movie Database API.
    - Integrated HTTP requests for token validation and login processes.
    - Removed redundant Firebase Auth comments.
    - Updated imports to include necessary HTTP and JSON handling libraries.
    - Wrapped API key and host details in a separate file for better security and maintainability.
    
    This refactor aims to replace Firebase authentication with The Movie Database API for a more tailored user authentication experience within the app.

--- lib/controller/c_auth.dart
+++ lib/controller/c_auth.dart
@@ -1,5 +1,9 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
+import 'package:filmo_app/api/app_api_key.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import '../models/auth/m_auth_user.dart';
 import '../utils/constant/app_icons.dart';
 
@@ -28,8 +32,6 @@ class ControllerAuth extends ChangeNotifier {
     currentPass = value;
   }
 
-  // * Firebase Auth
-
   bool loading = false;
   String errorMessage = '';
 
@@ -41,5 +43,49 @@ class ControllerAuth extends ChangeNotifier {
   set updateMessage(String value) {
     errorMessage = value;
     notifyListeners();
+  }
+
+  Future<String> createRequestToken() async {
+    final response = await http.get(Uri.parse(AppApiKey.host));
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      return data['request_token'];
+    } else {
+      throw Exception('Failed to create request token');
+    }
+  }
+
+  Future<bool?> createSession(String requestToken) async {
+    final url = Uri.parse(
+        'https://api.themoviedb.org/3/authentication/token/validate_with_login');
+    final headers = {
+      'Authorization':
+          'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyOWIxMGFkZWEzYzRhMTBlOGFiNDY0MmFmODNjZjViZSIsIm5iZiI6MTcyMzIxMjQzOS43MTA0MjksInN1YiI6IjY2YjFmNjVkM2RjNGJmZjEzYjc1NzE4NCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.-OwMevthWvlaaFNK6hEzYSub5-NTmqUYka6e80AtL08',
+      'accept': 'application/json',
+      'content-type': 'application/json',
+    };
+    final body = jsonEncode({
+      'username': userAuth.userName,
+      'password': userAuth.password,
+      'request_token': requestToken,
+    });
+
+    final response = await http.post(url, headers: headers, body: body);
+
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      log(name: "stats of login", data['success'].toString());
+      return data['success'];
+    } else {
+      log('Error: ${response.statusCode} - ${response.body}');
+      return null;
+    }
+  }
+
+  Future<bool?> login() async {
+    String requestToken = await createRequestToken();
+    bool? success = await createSession(requestToken);
+    return success;
   }
 }
