4502bb43b0fdfb3327a7b9d9898a21bcdf15ae8d
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-10 16:49:28 +0300

    Refactor main.dart to use MultiProvider for state management
    Introduce MultiProvider in the main function to manage state more effectively. This change includes the addition of a provider list that initializes the ControllerAuth class using ChangeNotifierProvider. This setup enhances the app's scalability and maintainability by centralizing state management and dependency injection. The previous direct invocation of AppStart is replaced to accommodate this new structure, ensuring that all necessary providers are loaded before the app starts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
+import 'package:filmo_app/controller/c_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider/single_child_widget.dart';
 
 import 'app_start/app_start.dart';
 
 void main() {
-  runApp(const AppStart());
+  runApp(MultiProvider(providers: providerList, child: const AppStart()));
 }
+
+List<SingleChildWidget> providerList = [
+  ChangeNotifierProvider(create: (_) => ControllerAuth()),
+];
