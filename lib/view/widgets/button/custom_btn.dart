834a3da3eb60992f692b94930e886b5bb1bd8995
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-10 17:09:24 +0300

    Add Custom Button Widget for Enhanced UI Interactivity
    This commit introduces a new custom button widget, `CustomBtn`, designed to
    enhance user interaction within the application's UI. The widget is
    implemented in the `lib/view/widgets/button/custom_btn.dart` file and includes
    several customizable properties such as height, title text, and an onTap
    callback function.
    
    The `CustomBtn` widget leverages the application's theme utilities for colors
    and text styles, ensuring consistency across the UI. It features a customizable
    height, defaulting to 70 if not specified, and a flexible width set to 100. The
    button's container is decorated with a border radius of 20, giving it a rounded
    appearance, and it uses the `AppColors.blueAccent` for its background color.
    
    This addition aims to standardize button styles and improve the overall user
    experience by providing a reusable and visually appealing button component.

---
+++ lib/view/widgets/button/custom_btn.dart
@@ -0,0 +1,42 @@
+import 'package:filmo_app/utils/theme/app_colors.dart';
+import 'package:filmo_app/utils/theme/app_text_style.dart';
+import 'package:flutter/material.dart';
+
+class CustomBtn extends StatelessWidget {
+  const CustomBtn({
+    super.key,
+    this.height,
+    required this.title,
+    required this.onTap,
+  });
+
+  final double? height;
+  final String title;
+  final void Function() onTap;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      splashColor: AppColors.darkGrey,
+      borderRadius: BorderRadius.circular(20),
+      child: Container(
+        width: 100,
+        height: height ?? 70,
+        margin: EdgeInsets.symmetric(
+          horizontal: 5,
+          vertical: 5,
+        ),
+        padding: EdgeInsets.zero,
+        alignment: Alignment.center,
+        decoration: BoxDecoration(
+          color: AppColors.blueAccent,
+          borderRadius: BorderRadius.circular(20),
+        ),
+        child: Text(
+          title.toUpperCase(),
+          style: AppTextStyle.h4Medium16(context),
+        ),
+      ),
+    );
+  }
+}
