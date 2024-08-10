63826eaec7659f1f4c6f0e955d0e55f2ef80f794
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-10 17:06:52 +0300

    Add comprehensive text styles for app theme in Flutter
    This commit introduces a new file, `app_text_style.dart`, within the `lib/utils/theme` directory, which centralizes the definition of various text styles used throughout the application. Each text style is categorized by header level (h1 to h7) and further differentiated by font weight (semibold, medium, regular), providing a consistent and scalable approach to text styling across the app.
    
    The text styles are defined using the Montserrat font family and include font sizes ranging from 28.0 to 10.0, covering a wide range of typographic needs. This centralized approach to text styling enhances maintainability and ensures uniformity in the app's visual presentation.

---
+++ lib/utils/theme/app_text_style.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+
+class AppTextStyle {
+  static TextStyle h1Semibold28(BuildContext context) {
+    return const TextStyle(
+      fontSize: 28.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h1Medium28(BuildContext context) {
+    return const TextStyle(
+      fontSize: 28.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h1Regular28(BuildContext context) {
+    return const TextStyle(
+      fontSize: 28.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h2Semibold24(BuildContext context) {
+    return const TextStyle(
+      fontSize: 24.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h2Medium24(BuildContext context) {
+    return const TextStyle(
+      fontSize: 24.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h2Regular24(BuildContext context) {
+    return const TextStyle(
+      fontSize: 24.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h3Semibold18(BuildContext context) {
+    return const TextStyle(
+      fontSize: 18.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h3Medium18(BuildContext context) {
+    return const TextStyle(
+      fontSize: 18.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h3Regular18(BuildContext context) {
+    return const TextStyle(
+      fontSize: 18.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h4Semibold16(BuildContext context) {
+    return const TextStyle(
+      fontSize: 16.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h4Medium16(BuildContext context) {
+    return const TextStyle(
+      fontSize: 16.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h4Regular16(BuildContext context) {
+    return const TextStyle(
+      fontSize: 16.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h5Semibold14(BuildContext context) {
+    return const TextStyle(
+      fontSize: 14.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h5Medium14(BuildContext context) {
+    return const TextStyle(
+      fontSize: 14.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h5Regular14(BuildContext context) {
+    return const TextStyle(
+      fontSize: 14.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h6Semibold12(BuildContext context) {
+    return const TextStyle(
+      fontSize: 12.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h6Medium12(BuildContext context) {
+    return const TextStyle(
+      fontSize: 12.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h6Regular12(BuildContext context) {
+    return const TextStyle(
+      fontSize: 12.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+
+  static TextStyle h7Semibold10(BuildContext context) {
+    return const TextStyle(
+      fontSize: 10.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w600,
+    );
+  }
+
+  static TextStyle h7Medium10(BuildContext context) {
+    return const TextStyle(
+      fontSize: 10.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w500,
+    );
+  }
+
+  static TextStyle h7Regular10(BuildContext context) {
+    return const TextStyle(
+      fontSize: 10.0,
+      fontFamily: 'Montserrat',
+      fontWeight: FontWeight.w400,
+    );
+  }
+}
