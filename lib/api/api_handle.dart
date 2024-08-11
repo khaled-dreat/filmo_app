126a2c6d7724698cd91eb67194092f946908adff
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-12 00:20:39 +0300

    Refactor API handling to include popular movies and correct model imports
    - Corrected import paths for NowPlaying and PopularMovies models.
    - Updated method names to follow Dart's naming conventions.
    - Added a new method to fetch popular movies.
    
    This refactor ensures that the API handling layer correctly interacts with the updated models and includes functionality to fetch popular movies, enhancing the application's feature set.

--- lib/api/api_handle.dart
+++ lib/api/api_handle.dart
@@ -1,9 +1,11 @@
 import 'package:dartz/dartz.dart';
-import 'package:filmo_app/models/now/now/now_playing.dart';
+import 'package:filmo_app/models/now_playing/m_now_playing_list.dart';
 
+import '../models/popular_movies/m_popular_movies_list.dart';
 import '../utils/failure/app_failure.dart';
 
 abstract class ApiHandle {
   // * get list app type social
-  Future<Either<Failure, NowPlayingModel>> NowPlayingFilmes();
+  Future<Either<Failure, NowPlayingListModel>> nowPlaying();
+  Future<Either<Failure, PopularMoviesListModel>> popularMovies();
 }
