2bc83a5e14e847cfeff192355fb2e6588a1f278e
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-12 22:33:24 +0300

    Refactor SectionHeader widget for enhanced UI and functionality

--- lib/view/home/widgets/section_headar.dart
+++ lib/view/home/widgets/section_headar.dart
@@ -1,30 +1,52 @@
+import 'package:filmo_app/utils/constant/app_images.dart';
+import 'package:filmo_app/utils/theme/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:svg_flutter/svg_flutter.dart';
+
+import '../../../utils/theme/app_text_style.dart';
+import 'image_profile.dart';
 
 class SectionHeadar extends StatelessWidget {
   const SectionHeadar({
-    Key? key,
+    super.key,
     required this.title,
     this.onPressed,
-  }) : super(key: key);
+  });
 
   final String title;
   final void Function()? onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          title,
-          //    style: AppTheme.h5(context),
-        ),
-        //  IconButton(
-        //      onPressed: onPressed,
-        //      icon: Icon(AppLang.currentLang(context) == kEn
-        //          ? Icons.keyboard_double_arrow_right_outlined
-        //          : Icons.keyboard_double_arrow_left_outlined))
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+              height: 40,
+              padding: const EdgeInsets.only(right: 10),
+              child: const ImageProfile()),
+          RichText(
+            textAlign: TextAlign.start,
+            text: TextSpan(
+              text: "Hello, Smith\n",
+              style: AppTextStyle.h5Semibold14(context).copyWith(fontSize: 24),
+              children: <TextSpan>[
+                TextSpan(
+                  style: AppTextStyle.h6Medium12(context).copyWith(
+                    fontSize: 14,
+                    color: AppColors.grey,
+                  ),
+                  text: "Let’s stream your favorite movie",
+                ),
+              ],
+            ),
+          ),
+          const Spacer(),
+          SvgPicture.asset(Assets.imgWishlist)
+        ],
+      ),
     );
   }
 }
