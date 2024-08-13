564be85f0f1cd8b3cfdc684e6cc3762691e92f0c
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-13 23:17:01 +0300

    Add new image assets for avatar and wishlist icons

--- lib/utils/constant/app_images.dart
+++ lib/utils/constant/app_images.dart
@@ -1,7 +1,16 @@
 class Assets {
   Assets._();
+  
+  /// Assets for imgAvatar
+  /// assets/img/Avatar.png
+  static const String imgAvatar = "assets/img/Avatar.png";
 
   /// Assets for imgLogo
   /// assets/img/logo.png
   static const String imgLogo = "assets/img/logo.png";
+
+  /// Assets for imgWishlist
+  /// assets/img/Wishlist.svg
+  static const String imgWishlist = "assets/img/Wishlist.svg";
 }
+
