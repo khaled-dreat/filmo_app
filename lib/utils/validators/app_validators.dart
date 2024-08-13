6f4b77637ee2b3e53a184075635d74910640a9fd
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-13 23:17:30 +0300

    Refactor validator messages to use const instead of final

--- lib/utils/validators/app_validators.dart
+++ lib/utils/validators/app_validators.dart
@@ -2,12 +2,12 @@ import 'package:validators/validators.dart' as validators;
 
 class AppValidators {
   // * message Error
-  static final String messageEnterValue = "Please enter a value";
-  static final String messageCorrectEmail =
+  static const String messageEnterValue = "Please enter a value";
+  static const String messageCorrectEmail =
       "please write an email address by way correct.";
-  static final String messageCorrectPass =
+  static const String messageCorrectPass =
       "A password of no less than 6 characters and no more than 12.";
-  static final String messageConfirmPass =
+  static const String messageConfirmPass =
       "Your password and confirmation password don't match.";
 
   static String? isUserName(String? value) => templateValidator(
