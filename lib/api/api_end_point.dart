f6b7bd1cbfd01855db978fd32b01dc2b7872f889
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-14 12:01:18 +0300

    Refactor API endpoint methods and add genre list retrieval

--- lib/api/api_end_point.dart
+++ lib/api/api_end_point.dart
@@ -4,6 +4,7 @@ import 'dart:developer';
 import 'package:dartz/dartz.dart';
 import 'package:filmo_app/api/api_handle.dart';
 import 'package:filmo_app/api/app_api_key.dart';
+import 'package:filmo_app/models/genres/m_genres_list.dart';
 import 'package:filmo_app/models/popular_movies/m_popular_movies_list.dart';
 import 'package:filmo_app/utils/failure/app_failure.dart';
 import 'package:http/http.dart' as http;
@@ -12,7 +13,7 @@ import '../models/now_playing/m_now_playing_list.dart';
 
 class ApiEndPoint extends ApiHandle {
   @override
-  Future<Either<Failure, NowPlayingListModel>> nowPlaying() async {
+  Future<Either<Failure, NowPlayingListModel>> getNowPlayingList() async {
     final headers = {
       'Authorization': AppApiKey.readAccessToken,
       'accept': 'application/json',
@@ -33,7 +34,7 @@ class ApiEndPoint extends ApiHandle {
   }
 
   @override
-  Future<Either<Failure, PopularMoviesListModel>> popularMovies() async {
+  Future<Either<Failure, PopularMoviesListModel>> getPopularMoviesList() async {
     final headers = {
       'Authorization': AppApiKey.readAccessToken,
       'accept': 'application/json',
@@ -48,6 +49,27 @@ class ApiEndPoint extends ApiHandle {
       Map<String, dynamic> data = jsonDecode(response.body);
       log(data.toString());
       return Right(PopularMoviesListModel.fromJson(data));
+    } else {
+      return Left(ServerFailure.fromHttpException(response));
+    }
+  }
+
+  @override
+  Future<Either<Failure, GenresListModel>> getGenresList() async {
+    final headers = {
+      'Authorization': AppApiKey.readAccessToken,
+      'accept': 'application/json',
+    };
+    final uri = Uri.parse(AppApiKey.urlBuilder(value: AppApiKey.genre));
+    final response = await http.get(
+      uri,
+      headers: headers,
+    );
+
+    if (response.statusCode == 200) {
+      Map<String, dynamic> data = jsonDecode(response.body);
+      log(data.toString());
+      return Right(GenresListModel.fromJson(data));
     } else {
       return Left(ServerFailure.fromHttpException(response));
     }
