eef32db88624252fe571260e6ef8666cb38adfd7
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-17 16:48:54 +0300

    Add search functionality to API handle

--- lib/api/api_handle.dart
+++ lib/api/api_handle.dart
@@ -1,12 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:filmo_app/models/genres/m_genres_list.dart';
 import 'package:filmo_app/models/now_playing/m_now_playing_list.dart';
-
 import '../models/popular_movies/m_popular_movies_list.dart';
+import '../models/search/m_search_list.dart';
 import '../utils/failure/app_failure.dart';
 
 abstract class ApiHandle {
   Future<Either<Failure, NowPlayingListModel>> getNowPlayingList();
   Future<Either<Failure, PopularMoviesListModel>> getPopularMoviesList();
   Future<Either<Failure, GenresListModel>> getGenresList();
+  Future<Either<Failure, SearchListModel>> search({required String srhText});
 }
