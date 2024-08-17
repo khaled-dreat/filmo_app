b5ff762b1b518c9f95aaf01730e3547bae55389e
Khaled Dreat <dreatkhaled@gmail.com>
2024-08-17 16:53:46 +0300

    Refactor HomeView to include SearchView and improve navigation

--- lib/view/home/home_view.dart
+++ lib/view/home/home_view.dart
@@ -1,6 +1,7 @@
-import 'package:filmo_app/utils/theme/app_colors.dart';
+import 'package:filmo_app/view/search/search_view.dart';
 import 'package:flutter/material.dart';
 
+import 'widgets/custom_selected_icon_bottom_navigation_bar.dart';
 import 'widgets/home_view_body.dart';
 
 class HomeView extends StatefulWidget {
@@ -12,35 +13,42 @@ class HomeView extends StatefulWidget {
 }
 
 class _HomeViewState extends State<HomeView> {
-  int currentIndex = 1;
+  int currentIndex = 0;
+
+  final List<Widget> pages = [
+    const HomeViewBody(),
+    const SearchView(),
+    const HomeViewBody(),
+  ];
+  List<BottomNavigationBarItem> items = const [
+    BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: '',
+        activeIcon: CustomSelectedIconBottomNavigationBar(
+          icon: Icons.home,
+          label: "Home",
+        )),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.search),
+        label: '',
+        activeIcon: CustomSelectedIconBottomNavigationBar(
+          icon: Icons.search,
+          label: "Search",
+        )),
+    BottomNavigationBarItem(
+        icon: Icon(Icons.person),
+        label: '',
+        activeIcon: CustomSelectedIconBottomNavigationBar(
+          icon: Icons.person,
+          label: "Profile",
+        )),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.shifting,
-        items: const [
-          BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: '',
-              activeIcon: CustomSelectedIconBottomNavigationBar(
-                icon: Icons.home,
-                label: "Home",
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: '',
-              activeIcon: CustomSelectedIconBottomNavigationBar(
-                icon: Icons.search,
-                label: "Search",
-              )),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person),
-              label: '',
-              activeIcon: CustomSelectedIconBottomNavigationBar(
-                icon: Icons.person,
-                label: "Profile",
-              )),
-        ],
+        items: items,
         currentIndex: currentIndex,
         onTap: (index) {
           setState(() {
@@ -48,43 +56,9 @@ class _HomeViewState extends State<HomeView> {
           });
         },
       ),
-      body: HomeViewBody(),
-    );
-  }
-}
-
-class CustomSelectedIconBottomNavigationBar extends StatelessWidget {
-  const CustomSelectedIconBottomNavigationBar(
-      {super.key, required this.icon, required this.label});
-  final IconData icon;
-  final String label;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.sizeOf(context).width * .3,
-      height: MediaQuery.sizeOf(context).height * .05,
-      decoration: BoxDecoration(
-        color: AppColors.soft,
-        borderRadius: BorderRadius.circular(20),
-      ),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Icon(
-            icon,
-            color: Colors.cyan,
-          ),
-          SizedBox(
-            width: 5,
-          ),
-          Text(
-            label,
-            style: TextStyle(
-              color: Colors.cyan,
-            ),
-          ),
-        ],
+      body: IndexedStack(
+        index: currentIndex,
+        children: pages,
       ),
     );
   }
